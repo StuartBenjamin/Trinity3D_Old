implemented corrected calculation of dlogGamma, this appears to have fixed the explicit oscillation bug

run.py
@@ -21,15 +21,16 @@ rho_axis = np.linspace(0,rho_edge,N) # radial axis
 drho = 1/N # temp
 # sample profile initial conditions
 n = (n_core-n_edge)*(1 - (rho_axis/rho_edge)**2) + n_edge
-#n = n_core*(1 - rho_axis**2) #+ n_edge  # simple expression
-#n = n_edge * np.ones(N)   ## constant init
 T0  = 2 # constant temp profile 
 T = T0 * np.ones(N)
 
 ### Set up time controls
 alpha = 1          # explicit to implicit mixer
-dtau  = 10         # step size 
-N_steps  = 10       # total Time = dtau * N_steps
+dtau  = 1e5         # step size 
+N_steps  = 1000       # total Time = dtau * N_steps
+#alpha = 0          # explicit to implicit mixer
+#dtau  = 1e-3         # step size 
+#N_steps  = 10000       # total Time = dtau * N_steps
 N_prints = 10
 N_step_print = N_steps // N_prints   # how often to print # thanks Sarah!
 #N_step_print = 100   # how often to print

trinity_lib.py
@@ -41,8 +41,8 @@ class profile():
 
         # pre-calculate gradients, half steps, or full steps
         if (grad):
-            self.grad     =  profile(self.gradient())
-            self.grad_log =  profile(self.log_gradient())
+            self.grad     =  profile(self.gradient(), half=half, full=full)
+            self.grad_log =  profile(self.log_gradient(), half=half, full=full)
 
         if (half): # defines half step
             self.plus  = profile(self.halfstep_pos())
@@ -144,20 +144,26 @@ flux_slope = 1
 critical_gradient = 1.5
 D_neo = .1 # neoclassical particle diffusion 
 
+# toy function for calculating Gamma (as ReLU)
+# also returns d log ( Gamma ) / d ( 1 / Ln ), with ambiguous normalization
 def calc_Gamma(density,debug=False):
     Ln_inv     = -density.grad_log.profile # Lninv
     G_turb     = np.vectorize(mf.ReLU)(Ln_inv, a=critical_gradient, m=flux_slope) 
     G_neo      = - D_neo * density.grad.profile
-    dlogG_turb = np.vectorize(mf.Step)(Ln_inv, a=critical_gradient, m=flux_slope)
+#    dlogG_turb = np.vectorize(mf.Step)(Ln_inv, a=critical_gradient, m=flux_slope) 
+    #dlogG_turb = np.vectorize(mf.Step)(Ln_inv, a=critical_gradient, m=flux_slope) / density.profile
     ### Is this actually dlogGamma? if Gamma is ReLU, dGamma is step
     #dlogG_neo  = D_neo * density.grad.profile # negligible
+    #dlogGamma = profile(dlogG_turb,grad=True,half=True)
 
-    gamma = G_turb
-    gamma = G_neo
-    #gamma = G_turb + G_neo
+    # for debugging turublent and neoclassical transport 
+        # for debugging turublent and neoclassical transport separately
+#    gamma = G_turb
+#    gamma = G_neo
+    gamma = G_turb + G_neo
     
     Gamma     = profile(gamma,grad=True,half=True)
-    dlogGamma = profile(dlogG_turb,grad=True,half=True)
+    dlogGamma = Gamma.grad_log
     
     if (debug):
         Gamma.plot(new_fig=True,label=r'$\Gamma$')