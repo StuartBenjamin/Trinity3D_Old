071e23ebb9471cc1b02be27c3b3ab7ed243d7397
TacitusQ <tq2115@columbia.edu>
2021-11-29 16:47:24 -0500

    creating saved state, from before trinity engine was implmented, saved to old/1129

---
+++ old/1129-nevol-works/models.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+### this library contains model functons for flux behavior
+
+def ReLU(x,a=0.5,m=1):
+    # piecewise-linear function
+    # can model Gamma( critical temperature gradient scale length ), for example
+    if (x < a):
+        return 0
+    else:
+        return m*(x-a)
+
+def Step(x,a=0.5,m=1):
+    # derivative of ReLU (is just step function)
+    if (x < a):
+        return 0
+    else:
+        return m
+
+
+# for a particle source
+def Gaussian(x,sigma=.3,A=2):
+    w = - (x/sigma)**2  / 2
+    return A * np.e ** w
