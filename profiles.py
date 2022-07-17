afd2e300f6900b554a3d46fdf86e127035edb514
Tony Qian <tqian@pppl.gov>
2022-07-17 18:10:10 -0400

    adding color gradients to tools/profile-plot.py

--- profiles.py
+++ profiles.py
@@ -254,8 +254,8 @@ class Flux_coefficients():
         with np.errstate(divide='ignore', invalid='ignore'):
             dLogZp = np.nan_to_num( dZp / Zp )
 
-        import pdb
-        pdb.set_trace()
+#        import pdb
+#        pdb.set_trace()
 
         Cp = - norm * (x / xp**2) * Yp * dLogZp
         return Profile(Cp)
