807fa820b8b6be1155699abd77b01072a1f9eae7
tmqian <tqian@pppl.gov>
2022-09-13 12:22:45 -0400

    adding 2nd row of profiles to power-balance-plot.py

--- tools/power-balance-plot.py
+++ tools/power-balance-plot.py
@@ -24,16 +24,19 @@ n     =      np.array( data['n'     ] )
 pi    =      np.array( data['pi'    ] ) 
 pe    =      np.array( data['pe'    ] ) 
 
+Ti = pi/n
+Te = pe/n
+
 profile_data = data['profiles']
 N_rho    = profile_data['N_radial']
 rho_edge = profile_data['rho_edge']
 axis        = np.linspace(0,rho_edge,N_rho) # radial axis
 
 # unused
-P_fusion_Wm3 = np.array( data['P_fusion_Wm3'] )
-P_brems_Wm3 = np.array( data['P_brems_Wm3'] )
-fusion_rate = np.array( data['fusion_rate'] )
-nu_ei_Hz = np.array( data['nu_ei_Hz'] )
+#P_fusion_Wm3 = np.array( data['P_fusion_Wm3'] )
+#P_brems_Wm3 = np.array( data['P_brems_Wm3'] )
+#fusion_rate = np.array( data['fusion_rate'] )
+#nu_ei_Hz = np.array( data['nu_ei_Hz'] )
 
 
 ###
@@ -59,41 +62,44 @@ def plot_power_balance(t=0):
 
     rax = axis[:-1]
 
-    fig,axs = plt.subplots(1,3, figsize=(12,4) )
-    axs[0].plot(rax, force_n      [t]     , '.-', label='turbulent particle flux')
-    axs[0].plot(rax, aux_source_n [t][:-1], '.-', label='auxiliary particle source')
+    fig,axs = plt.subplots(2,3, figsize=(12,8) )
+    axs[0,0].plot(rax, force_n      [t]     , '.-', label='turbulent particle flux')
+    axs[0,0].plot(rax, aux_source_n [t][:-1], '.-', label='auxiliary particle source')
 
-    axs[1].plot(rax, force_pi     [t]     , '.-', label='turbulent heat flux')
-    axs[1].plot(rax, Ei           [t][:-1], '.-', label='collisional heat exchange')
-    axs[1].plot(rax, Gi           [t][:-1], '.-', label='G term')
-    axs[1].plot(rax, Hi           [t][:-1], '.-', label='H term')
-    axs[1].plot(rax, P_fusion     [t][:-1], '.-', label='alpha heating')
-    axs[1].plot(rax, aux_source_pi[t][:-1], '.-', label='auxiliary ion heating')
+    axs[0,1].plot(rax, force_pi     [t]     , '.-', label='turbulent heat flux')
+    axs[0,1].plot(rax, Ei           [t][:-1], '.-', label='collisional heat exchange')
+    axs[0,1].plot(rax, Gi           [t][:-1], '.-', label='G term')
+    axs[0,1].plot(rax, Hi           [t][:-1], '.-', label='H term')
+    axs[0,1].plot(rax, P_fusion     [t][:-1], '.-', label='alpha heating')
+    axs[0,1].plot(rax, aux_source_pi[t][:-1], '.-', label='auxiliary ion heating')
 
-    axs[2].plot(rax, force_pe     [t]     , '.-', label='turbulent heat flux')
-    axs[2].plot(rax, Ee           [t][:-1], '.-', label='collisional heat exchange')
-    axs[2].plot(rax, Ge           [t][:-1], '.-', label='G term')
-    axs[2].plot(rax, He           [t][:-1], '.-', label='H term')
-    axs[2].plot(rax, P_brems      [t][:-1], '.-', label='bremstrahlung radiation') 
-    axs[2].plot(rax, aux_source_pe[t][:-1], '.-', label='auxiliary electron heating')
+    axs[0,2].plot(rax, force_pe     [t]     , '.-', label='turbulent heat flux')
+    axs[0,2].plot(rax, Ee           [t][:-1], '.-', label='collisional heat exchange')
+    axs[0,2].plot(rax, Ge           [t][:-1], '.-', label='G term')
+    axs[0,2].plot(rax, He           [t][:-1], '.-', label='H term')
+    axs[0,2].plot(rax, P_brems      [t][:-1], '.-', label='bremstrahlung radiation') 
+    axs[0,2].plot(rax, aux_source_pe[t][:-1], '.-', label='auxiliary electron heating')
 
     dn  = force_n[t]  + aux_source_n [t][:-1]                              
     dpi = force_pi[t] + aux_source_pi[t][:-1] + P_fusion[t][:-1] + Ei[t][:-1]
     dpe = force_pe[t] + aux_source_pe[t][:-1] + P_brems [t][:-1] + Ee[t][:-1] 
-    axs[0].plot(rax, dn , '--', color='gray', label=r'$\Delta n$')
-    axs[1].plot(rax, dpi, '--', color='gray', label=r'$\Delta p_i$')
-    axs[2].plot(rax, dpe, '--', color='gray', label=r'$\Delta p_e$')
+    axs[0,0].plot(rax, dn , '--', color='gray', label=r'$\Delta n$')
+    axs[0,1].plot(rax, dpi, '--', color='gray', label=r'$\Delta p_i$')
+    axs[0,2].plot(rax, dpe, '--', color='gray', label=r'$\Delta p_e$')
 
-    axs[0].set_title('n')
-    axs[1].set_title('pi')
-    axs[2].set_title('pe')
+    axs[0,0].set_title('n')
+    axs[0,1].set_title('pi')
+    axs[0,2].set_title('pe')
+
+    axs[0,0].legend(frameon=False)
+    axs[0,1].legend(frameon=False)
+    axs[0,2].legend(frameon=False)
+
+    axs[1,0].plot(axis, n [t] , '.-')
+    axs[1,1].plot(axis, pi[t] , '.-')
+    axs[1,2].plot(axis, pe[t] , '.-')
 
     plt.suptitle('t = {}'.format(t))
-
-    axs[0].legend(frameon=False)
-    axs[1].legend(frameon=False)
-    axs[2].legend(frameon=False)
-
 
 ## plot
 path = "tmp/"
