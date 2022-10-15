+ Major check (minor change), defining Trinity reference field Ba to match the normalizing field in GX + It appears that the relu bug from earlier was actually a real effect.   Multiplying by grad rho ~ 1/a for a < 1 increases the effective div Q.   As a result the step was larger, CFL was exceeded, and dt needed to be made smaller.   This explains why the bug appeared for test 1b and 1d but not test 2.

Geometry.py
@@ -106,7 +106,7 @@ class VmecReader():
         self.aminor      = get(f,'Aminor_p')
         self.Rmajor      = get(f,'Rmajor_p')
         self.volume      = get(f,'volume_p')
-        self.volavgB     = get(f,'volavgB')
+
 
         # 1D array
         self.xm          = get(f,'xm')
@@ -124,6 +124,12 @@ class VmecReader():
         self.bmnc        = get(f,'bmnc')
         self.bsupumnc    = get(f,'bsupumnc')
         self.bsupvmnc    = get(f,'bsupvmnc')
+        
+        # Get B field
+        self.phi  = get(f,'phi') # toroidal flux in SI webers (signed)
+        phiedge   = self.phi[-1]
+        B_norm = phiedge / (np.pi * self.aminor**2) # GX normalizes B_ref = phi/ pi a2
+        self.B_GX = np.abs(B_norm) 
 
         # save
         self.data = f

trinity_lib.py
@@ -439,7 +439,8 @@ class Trinity_Engine():
         vmec = VmecReader(path+wout)
         self.R_major = vmec.Rmajor
         self.a_minor = vmec.aminor
-        self.Ba      = vmec.volavgB
+        #self.Ba      = vmec.volavgB # replaced 10/15
+        self.Ba      = vmec.B_GX
 
         if run_fast:
             grho = np.ones(self.N_radial)
@@ -447,10 +448,13 @@ class Trinity_Engine():
 
         else:
             print("    post-processing for surface areas")
+
+            ## retired 10/14
             #vmec.calc_geometry(s_idx)
             #s_idx = np.array([ np.rint(r) for r in self.rho_axis**2 * vmec.ns ], int)
             #if s_idx[0] == 0:
             #    s_idx[0] += 1
+            ##
 
             vmec.calc_gradrho_area(self.rho_axis)
             area = vmec.surface_areas